9457149cd1a717e018b26c936c1988836d521d3c
Atharv Sharma <31643037+atharvsharma1998@users.noreply.github.com>
2023-02-18 19:49:21 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+package example;
+
+
+
+
+public class main {
+    
+    // This function performs XOR
+	public static int COMPUTERFUNCTION(int a, int b) {
+		return a^b;
+	}
+	
+ 
+  public static void main(String args[]) {
+	  
+	  System.out.println(COMPUTERFUNCTION(61,233));
+  	  
+	 
+  }
+}
